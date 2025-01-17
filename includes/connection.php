351057e923a60d845048a044aae884740c1d9f88
paiboonmt <paiboon500@gmail.com>
2025-01-17 13:28:37 +0700

    ok

--- includes/connection.php
+++ includes/connection.php
@@ -1,22 +1,9 @@
 <?php
-    // $servername = "localhost";
-    // $username = "admin";
-    // $password = "9itonly";
-    // $dbname = "member";
-    // $servername = "172.16.0.254";
-    // $username = "admin";
-    // $password = "9itonly";
-    // $dbname = "member";
-    // $servername = "146.190.92.118:3324";
-    // $username = "admin";
-    // $password = "tiger";
-    // $port = "3324";
-    // $dbname = "db";
-    
-    $servername = "27.254.145.135";
-    $username = "tiger_branch2";
+
+    $servername = "147.50.228.22";
+    $username = "tigersof_test";
     $password = "admin!@#$9itonly";
-    $dbname = "tigerbranch2";
+    $dbname = "tigersof_test";
 
     date_default_timezone_set('Asia/Bangkok');
     try {
